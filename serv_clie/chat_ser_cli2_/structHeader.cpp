2c7d68e3112cb1e599b40fad6a7bf0ad040d65bf
cachefish <780207419@qq.com>
2020-06-10 00:49:22 -0700

    test

--- serv_clie/chat_ser_cli2_/structHeader.cpp
+++ serv_clie/chat_ser_cli2_/structHeader.cpp
@@ -30,17 +30,18 @@ bool parseMessage(const std::string&input,int *type,std::string &outbuffer)
         return true;
     }else if(command == "Chat"){
         std::string chat=input.substr(pos+1);
-            std::string chat=input.substr(pos+1);
             if(chat.size()>256)
+            {
                 return false;
-                ChatInformation info;
-                info.infoLen = chat.size();
-                std::memcpy(&(info.information),chat.data(),chat.size());
-                auto buffer = reinterpret_cast<const char*>(&info);
-                outbuffer.assign(buffer,buffer+sizeof(info));
-                if(type)
-                    *type = MT_CHAT_INFO;
-                return true;
+            }
+            ChatInformation info;
+            info.infoLen = chat.size();
+            std::memcpy(&(info.information),chat.data(),chat.size());
+            auto buffer = reinterpret_cast<const char*>(&info);
+            outbuffer.assign(buffer,buffer+sizeof(info));
+            if(type)
+                *type = MT_CHAT_INFO;
+            return true;
     }
     return false;
 }
